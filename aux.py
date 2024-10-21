1ce62fbf358724211ddd60a9b477fd47d01a89b1
fishel <fishel@ut.ee>
2024-10-21 21:11:40 +0300

    testing

--- aux.py
+++ aux.py
@@ -8,10 +8,10 @@ def log(msg):
 
 def debug(msg):
     pass
-    # log("\n(DEBUG) " + msg)
+    ### log("\n(DEBUG) " + msg)
 
 def log_2dict(twod_dict, msg):
-    print(msg)
+
     for k1 in twod_dict:
         for k2 in twod_dict[k1]:
             log(f"DEBUG {msg}: {k1}, {k2} --> {twod_dict[k1][k2]}")
