4fded36a2a9b9511239e5ee408962e991fc2a7b4
fishel <fishel@ut.ee>
2024-12-30 21:31:25 +0200

    fixing accelerate's DP...

--- trainmodel.py
+++ trainmodel.py
@@ -220,7 +220,8 @@ class SwitchingAccelerator:
             batch, src_k, tgt_k = batch_with_idxs
 
             # inputs = self.accelerator.prepare(batch)
-            inputs = self.accelerator.prepare(batch)
+            inputs = self.accelerator.prepare(torch.nn.DataParallel(batch))
+            inputs = inputs.to(self.accelerator.device)
 
             encoder_vecs = self._encode(models[src_k], inputs)
 
