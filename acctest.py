68c2c2cc8e282e161f0c023c766c7a97b79026cc
fishel <fishel@ut.ee>
2025-01-01 15:23:00 +0200

    added test file for accelerate / DDP / etc.

--- acctest.py
+++ acctest.py
@@ -17,9 +17,9 @@ def main():
 
     log(f"Nr. of Cuda devices: {torch.cuda.device_count()}")
 
-    acc = Accelerator()
+    #acc = Accelerator()
 
-    #acc = Accelerator(mixed_precision="bf16", gradient_accumulation_steps=1, cpu=False, dataloader_config=DataLoaderConfiguration(split_batches=True, dispatch_batches=True))
+    acc = Accelerator(mixed_precision="bf16", gradient_accumulation_steps=1, cpu=False, dataloader_config=DataLoaderConfiguration(split_batches=True, dispatch_batches=True))
 
     log(f"Accelerator state: {str(acc.state)}")
 
