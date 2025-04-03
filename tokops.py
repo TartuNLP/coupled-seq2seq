cb9d694c94df36ce112a49349ecf98f9e9bde6c4
fishel <fishel@ut.ee>
2025-04-03 22:13:31 +0300

    NLLB tokenizer fails with added symbols, implemented an ugly hacl to alleviate that

--- tokops.py
+++ tokops.py
@@ -228,7 +228,7 @@ if __name__ == "__main__":
     with open(args.txt_file, "r", encoding="utf-8") as f:
         snts = f.read().split("\n")
 
-        toks = tokenizeit(tokenizer, snts, False, 1024)
+        toks = tokenizeit(tokenizer, snts, 1024, False)
 
         i = 0
 
