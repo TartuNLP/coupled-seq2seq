c276a794e331e8e71cab8a12c19c6e1a20e9a605
fishel <fishel@ut.ee>
2025-08-11 15:35:41 +0300

    updated prompts and implemented inference/training prompt filling

--- promptops.py
+++ promptops.py
@@ -1,42 +1,45 @@
 from aux import log
 
 PF_RAW = "raw"
-PF_SMUGRI = "smugri_lid_md"
+PF_RAWLINES = "rawlines"
+PF_SMUGRI_MT = "smugri_mt"
+PF_SMUGRI_LID = "smugri_lid"
 PF_ALPACA = "alpaca"
 
-INF_PROMPT_LID = "<|reserved_special_token_12|>{src_segm}<|reserved_special_token_13|>"
-INF_PROMPT_MT = INF_PROMPT_LID + "{src_lang}<|reserved_special_token_14|>{task} to {tgt_lang}<|reserved_special_token_15|>"
+SMUGRI_INF_PROMPT_LID = "<|reserved_special_token_12|>{src_segm}<|reserved_special_token_13|>"
 
-_TRAIN_PROMPT_PREF = "<|reserved_special_token_12|>{src_segm}<|reserved_special_token_13|>{src_lang}"
-_TRAIN_PROMPT_MID = "<|reserved_special_token_14|>{task} to {tgt_lang}<|reserved_special_token_15|>{tgt_segm}"
-_TRAIN_PROMPT_SUF = "<|reserved_special_token_16|><|end_of_text|>"
+_SMUGRI_INF_PROMPT_TMPMID = "<|reserved_special_token_14|>{task} to {tgt_lang}<|reserved_special_token_15|>"
+SMUGRI_INF_PROMPT_MT = SMUGRI_INF_PROMPT_LID + "{src_lang}" + _SMUGRI_INF_PROMPT_TMPMID
 
-TRAIN_PROMPT_PARA = _TRAIN_PROMPT_PREF + _TRAIN_PROMPT_MID + _TRAIN_PROMPT_SUF
-TRAIN_PROMPT_MONO = _TRAIN_PROMPT_PREF + _TRAIN_PROMPT_SUF
+_SMUGRI_TRAIN_PROMPT_PREF = SMUGRI_INF_PROMPT_LID + "{src_lang}"
+_SMUGRI_TRAIN_PROMPT_MID = _SMUGRI_INF_PROMPT_TMPMID + "{tgt_segm}"
+_SMUGRI_TRAIN_PROMPT_SUF = "<|reserved_special_token_16|><|end_of_text|>"
 
-ALPACA_PROMPT = ("Below is an instruction that describes a task, paired with an input that provides further context. "
+SMUGRI_PROMPT_TRAIN_PARA = _SMUGRI_TRAIN_PROMPT_PREF + _SMUGRI_TRAIN_PROMPT_MID + _SMUGRI_TRAIN_PROMPT_SUF
+SMUGRI_PROMPT_TRAIN_MONO = _SMUGRI_TRAIN_PROMPT_PREF + _SMUGRI_TRAIN_PROMPT_SUF
+
+ALPACA_PROMPT_INF = ("Below is an instruction that describes a task, paired with an input that provides further context. "
         "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n{output}")
+        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n")
+
+ALPACA_PROMPT_TRAIN = (ALPACA_PROMPT_INF + "{output}")
 
 
-def prep_prompt(data, prompt_format):
-    if prompt_format == PF_RAW:
+def prep_prompt(data, prompt_format, inference=False):
+    if prompt_format in {PF_RAW, PF_RAWLINES}:
         # data is a string, return it
         return data
 
-    elif prompt_format == PF_SMUGRI:
+    elif prompt_format in {PF_SMUGRI_MT, PF_SMUGRI_LID}:
         # data has src_segm, src_lang, tgt_lang, etc
-        return _prep_ljmf_entry(data)
+        return _prep_ljmf_entry(data, prompt_format, inference)
 
     elif prompt_format == PF_ALPACA:
         # data has instruction and input in it
-        return _prep_alpaca_entry(data)
+        return _prep_alpaca_entry(data, inference)
 
-
-def _prep_alpaca_entry(entry):
-    prompt = ALPACA_PROMPT.format(**entry)
-
-    return prompt
+    else:
+        raise NotImplementedError(f"Prompt format {prompt_format} is not implemented.")
 
 
 def tokenize_str(tokenizer, entry, add_eos=True, max_len=2000):
@@ -54,24 +57,24 @@ def tokenize_str(tokenizer, entry, add_eos=True, max_len=2000):
     return tokens
 
 
-def tokenize_for_inference(tokenizer, src_segm, src_lang=None, tgt_lang=None, task="translate", debug=False):
-    if task == "lid":
-        prompt = INF_PROMPT_LID.format(src_segm=src_segm)
-    elif task in {"translate", "approx-translate"}:
-        prompt = INF_PROMPT_MT.format(src_segm=src_segm, src_lang=src_lang, tgt_lang=tgt_lang, task=task)
+def _prep_alpaca_entry(entry, inference=False):
+    fmt = ALPACA_PROMPT_INF if inference else ALPACA_PROMPT_TRAIN
+    prompt = fmt.format(**entry)
+    return prompt
+
+
+def _prep_ljmf_entry(entry, fmt, inference=False):
+    if inference:
+        if fmt == PF_SMUGRI_MT:
+            prompt = SMUGRI_INF_PROMPT_MT.format(**entry)
+        elif fmt == PF_SMUGRI_LID:
+            prompt = SMUGRI_INF_PROMPT_LID.format(**entry)
+        else:
+            raise NotImplementedError(f"Prompt format {fmt} is not implemented.")
     else:
-        prompt = src_segm
-
-    if debug:
-        log(prompt)
-
-    return tokenize_str(tokenizer, prompt, add_eos=False)
-
-
-def _prep_ljmf_entry(entry):
-    if entry['task'] in {'translate', 'approx-translate'} and entry['tgt_segm'] and entry['tgt_lang']:
-        prompt = TRAIN_PROMPT_PARA.format(**entry)
-    else:
-        prompt = TRAIN_PROMPT_MONO.format(**entry)
+        if entry['task'] in {'translate', 'approx-translate'} and entry['tgt_segm'] and entry['tgt_lang']:
+            prompt = SMUGRI_PROMPT_TRAIN_PARA.format(**entry)
+        else:
+            prompt = SMUGRI_PROMPT_TRAIN_MONO.format(**entry)
 
     return prompt
