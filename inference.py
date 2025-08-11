a9e02642b112d70081f74bbfbd733ba219119d7d
fishel <fishel@ut.ee>
2025-08-11 15:36:17 +0300

    re-did inference to support any prompts via templates

---
+++ inference.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import promptops
+import torch
+
+from accelerate import Accelerator, data_loader
+from transformers import AutoModelForCausalLM, DataCollatorForLanguageModeling, AutoTokenizer
+
+from torch.utils.data import Dataset as TorchDataset, DataLoader
+from aux import CmdlineArgs, log
+from datetime import datetime
+
+
+def remove_prompt_from_output(att_mask, tokens, filler_id):
+    shape = list(att_mask.shape)
+    shape[1] = tokens.size(1)
+    a_padded = att_mask.new_zeros(*shape)
+
+    # copy original data
+    a_padded[:, :att_mask.size(1), ...] = att_mask
+
+    return (1-a_padded) * tokens + filler_id * a_padded
+
+
+def llm_generate(model, tokenizer, tok_batch, debug=False, max_len=3000, filler_id=128030):
+    tok_batch['input_ids'] = tok_batch['input_ids'].to(model.device)
+    tok_batch['attention_mask'] = tok_batch['attention_mask'].to(model.device)
+    start_time = datetime.now()
+
+    if debug:
+        log(f"Tokenized input: {tok_batch['input_ids']}")
+        log(f"Att mask: {tok_batch['attention_mask']}")
+
+    raw_outputs = model.generate(**tok_batch, tokenizer=tokenizer,
+                                 do_sample=False, num_beams=5, max_length=max_len, top_p=None,
+                                 temperature=None)
+
+    if debug:
+        log(f"Raw tokenized output: {raw_outputs}")
+        log(f"Raw tokens: {tokenizer.convert_ids_to_tokens(raw_outputs[0])}")
+
+    clean_outputs = remove_prompt_from_output(tok_batch['attention_mask'], raw_outputs, filler_id)
+
+    if debug:
+        end_time = datetime.now()
+        log(f"Cleaned output: {clean_outputs}")
+        log(f"This took: {end_time - start_time}")
+
+    return clean_outputs
+
+
+def predict(model, tokenizer, data_loader, accel, debug=False):
+    """
+    for inp_batch in data_loader:
+        these_outputs = llm_generate(model, tokenizer, inp_batch, debug=debug, max_len=3000)
+
+        all_outputs += these_outputs
+    """
+    outs_local = []
+
+    with torch.no_grad():
+        for idx, batch in enumerate(data_loader):
+            if idx % accel.num_processes == accel.process_index:
+                outputs = llm_generate(model, tokenizer, batch, debug=debug, max_len=3000)
+                outs_local += outputs
+
+    all_outputs = accel.gather_object(outs_local)
+
+    return all_outputs
+
+
+def read_input(path, format):
+    with open(path, 'r') as fh:
+        if format == promptops.PF_RAW:
+            result = fh.read()
+        elif format == promptops.PF_RAWLINES:
+            result = fh.readlines()
+        else:
+            result = json.load(fh)
+
+        return result
+
+
+class LazyTokenizingInferenceDataset(TorchDataset):
+    def __init__(self, texts, tokenizer, prompt_format, max_length=512, debug=False):
+        self.texts = texts
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.prompt_format = prompt_format
+        self.debug = debug
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        entry = self.texts[idx]
+
+        prompt = promptops.prep_prompt(entry, self.prompt_format, inference=True)
+        result = promptops.tokenize_str(self.tokenizer, prompt)
+
+        if self.debug:
+            log(f"Input: {prompt}")
+            log(f"Tokenized: {result}")
+
+        return result
+
+
+def get_data_loader(path, prompt_format, tokenizer, debug=False):
+    inputs = read_input(path, prompt_format)
+
+    dataset = LazyTokenizingInferenceDataset(inputs, tokenizer, prompt_format, debug=debug)
+
+    data_coll = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer,
+        mlm=False,
+        pad_to_multiple_of=None,  # helps performance; set None if you prefer exact lengths
+    )
+
+    data_loader = DataLoader(dataset, collate_fn=data_coll, batch_size=1)
+
+    return data_loader
+
+
+def _cmdline_args(inputs):
+    description = """Predict output for an input via prompting"""
+
+    pos_args = ["mdl_id", "input_file"]
+
+    #post-process the arguments
+    args = CmdlineArgs(description, pos_args, input_args=inputs,
+                       kw_arg_dict={"debug": False,
+                                    "prompt_format": promptops.PF_ALPACA})
+
+    log(f"Launched as {args}")
+
+    return args
+
+
+def and_i_called_this_function_do_main_too(iv):
+    args = _cmdline_args(iv)
+
+    acc = Accelerator()
+
+    model = AutoModelForCausalLM.from_pretrained(args.mdl_id,
+                                                 low_cpu_mem_usage=True,
+                                                 torch_dtype=torch.bfloat16,
+                                                 device_map={"": acc.device},
+                                                 attn_implementation="eager")
+    model.eval()
+
+    log(f"Device: {model.device}.", accelerator=acc)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.mdl_id)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = "<|reserved_special_token_100|>"
+
+    data_loader = get_data_loader(args.input_file, args.prompt_format, tokenizer, debug=args.debug)
+
+    log("Model loaded, starting to translate")
+    outputs = predict(model, tokenizer, data_loader, acc, debug=args.debug)
+
+    print("\n".join(outputs))
+
+    log("Done...")
+
+
+if __name__ == "__main__":
+    input_values = sys.argv[1:]
+
+    and_i_called_this_function_do_main_too(input_values)
