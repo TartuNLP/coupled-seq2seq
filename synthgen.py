b4e9f4ad774c8c1f295eaac80ba8cb4db18b1cac
fishel <fishel@ut.ee>
2025-09-01 15:03:27 +0300

    filtering manually

--- synthgen.py
+++ synthgen.py
@@ -145,6 +145,8 @@ def do_something_else_without_global_ctx():
 
 
 def lets_do_some_filtering():
+    c = 0
+    d = 0
     for f in sys.argv[1:]:
         with open(f, 'r') as fh_in:
             data = json.load(fh_in)
@@ -153,13 +155,16 @@ def lets_do_some_filtering():
             out_l = float(len(entry['hyp-output']))
 
             r = in_l / out_l if in_l > out_l else out_l /in_l
+            d += 1
 
             if r > 3:
                 print(f"RATIO: {entry}")
+                c += 1
 
             if entry['hi_segm'] == entry['hyp-output']:
                 print(f"EQ {entry['hi_segm']}")
-
+                c += 1
+    print(f"Filtered {c} out of {d} entries")
 
 if __name__ == "__main__":
     #do_something_without_global_ctx()
